c293893b1fda3ac9663f08b2eae8935e87d9d9ca
えいゆう <beautifuleiyuu@gmail.com>
2015-10-11 07:48:54 +0900

    レギュレーションPOST

--- src/Lain/OneNightWerewolfBundle/Controller/RegulationController.php
+++ src/Lain/OneNightWerewolfBundle/Controller/RegulationController.php
@@ -4,6 +4,9 @@ namespace Lain\OneNightWerewolfBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Routing\ClassResourceInterface;
+use Lain\OneNightWerewolfBundle\Entity\Regulation;
+use Lain\OneNightWerewolfBundle\Entity\RoleCount;
+use Symfony\Component\HttpFoundation\Request;
 
 class RegulationController extends FOSRestController implements ClassResourceInterface
 {
@@ -19,5 +22,23 @@ class RegulationController extends FOSRestController implements ClassResourceInt
         return $this->getDoctrine()
             ->getRepository('LainOneNightWerewolfBundle:Regulation')
             ->find($regulationId);
+    }
+
+    public function postAction(Request $request) {
+        $content = json_decode($request->getContent(), true);
+        $regulation = new Regulation();
+        $roleRepository = $this->getDoctrine()->getRepository('LainOneNightWerewolfBundle:Role');
+        foreach($content['role_counts'] as $roleInfo) {
+            $roleCount = new RoleCount();
+            $role = $roleRepository->find($roleInfo['id']);
+            $roleCount->setRole($role);
+            $roleCount->setCount($roleInfo['count']);
+            $roleCount->setRegulation($regulation);
+            $regulation->addRoleCount($roleCount);
+        }
+        $objectManager = $this->getDoctrine()->getManager();
+        $objectManager->persist($regulation);
+        $objectManager->flush();
+        return $regulation;
     }
 }
