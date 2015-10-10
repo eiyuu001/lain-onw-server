445d59ce57874dd25c85e99e88bf5fe9033356e8
えいゆう <beautifuleiyuu@gmail.com>
2015-10-11 04:48:05 +0900

    doctrinefixturesbundleを追加

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Lain\OneNightWerewolfBundle\LainOneNightWerewolfBundle(),
             new \FOS\RestBundle\FOSRestBundle(),
             new \JMS\SerializerBundle\JMSSerializerBundle(),
+            new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
