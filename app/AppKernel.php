e34df15af60289bc53b9977a887c6bff1c321c2d
えいゆう <beautifuleiyuu@gmail.com>
2015-10-11 00:48:39 +0900

    fosrestbundleとjmsserializerbundleを追加

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Lain\OneNightWerewolfBundle\LainOneNightWerewolfBundle(),
+            new \FOS\RestBundle\FOSRestBundle(),
+            new \JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
