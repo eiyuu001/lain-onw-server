e78f8e8b8b829fd4ba5d9e1d8f814413bfa58633
えいゆう <beautifuleiyuu@gmail.com>
2015-11-01 05:19:04 +0900

    不要なファイルを削除

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Lain\OneNightWerewolfBundle\LainOneNightWerewolfBundle(),
             new \FOS\RestBundle\FOSRestBundle(),
             new \JMS\SerializerBundle\JMSSerializerBundle(),
