026f0f50a4895837d94a4d900dd6f3d39fc7a72c
えいゆう <beautifuleiyuu@gmail.com>
2015-12-20 05:32:21 +0900

    RoleのAPI仕様を記述

--- src/Lain/OneNightWerewolfBundle/Controller/RoleController.php
+++ src/Lain/OneNightWerewolfBundle/Controller/RoleController.php
@@ -5,15 +5,31 @@ namespace Lain\OneNightWerewolfBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 use Lain\OneNightWerewolfBundle\Controller\Traits\EntityGettable;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class RoleController extends FOSRestController implements ClassResourceInterface
 {
     use EntityGettable;
 
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Returns a collection of Role"
+     * )
+     */
     public function cgetAction() {
         return $this->getRoles();
     }
 
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Returns a Role object specified by roleId",
+     *  requirements={
+     *      {"name"="roleId", "dataType"="integer", "requirement"="\d+", "description"="role id"}
+     *  }
+     * )
+     */
     public function getAction($roleId) {
         return $this->getRole($roleId);
     }
