b41c45f2be1f44426e288a1f61c6735b1d380891
えいゆう <beautifuleiyuu@gmail.com>
2015-12-27 06:56:43 +0900

    PlayerのApiDocを追加

--- src/Lain/OneNightWerewolfBundle/Controller/PlayerController.php
+++ src/Lain/OneNightWerewolfBundle/Controller/PlayerController.php
@@ -5,15 +5,31 @@ namespace Lain\OneNightWerewolfBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 use Lain\OneNightWerewolfBundle\Controller\Traits\EntityGettable;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class PlayerController extends FOSRestController implements ClassResourceInterface
 {
     use EntityGettable;
 
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Returns a collection of Player"
+     * )
+     */
     public function cgetAction() {
         return $this->getPlayers();
     }
 
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Returns a Player object specified by playerId",
+     *  requirements={
+     *      {"name"="playerId", "dataType"="integer", "requirement"="\d+", "description"="player id"}
+     *  }
+     * )
+     */
     public function getAction($playerId) {
         return $this->getPlayer($playerId);
     }
