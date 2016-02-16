49bd55c3cbfc04abc143000a5624ce8223912d16
えいゆう <beautifuleiyuu@gmail.com>
2016-02-17 02:36:52 +0900

    各種アクションの可否をロールマスタテーブルに移動

--- src/Lain/OneNightWerewolfBundle/DataFixtures/ORM/LoadRoleData.php
+++ src/Lain/OneNightWerewolfBundle/DataFixtures/ORM/LoadRoleData.php
@@ -21,9 +21,15 @@ class LoadRoleData implements FixtureInterface, OrderedFixtureInterface
                 'roles' => [
                     [
                         'name' => '人狼',
+                        'votable' => true,
+                        'peepable' => false,
+                        'swappable' => false,
                     ],
                     [
                         'name' => '狂人',
+                        'votable' => true,
+                        'peepable' => false,
+                        'swappable' => false,
                     ],
                 ]
             ],
@@ -32,12 +38,21 @@ class LoadRoleData implements FixtureInterface, OrderedFixtureInterface
                 'roles' => [
                     [
                         'name' => '村人',
+                        'votable' => true,
+                        'peepable' => false,
+                        'swappable' => false,
                     ],
                     [
                         'name' => '占い師',
+                        'votable' => true,
+                        'peepable' => true,
+                        'swappable' => false,
                     ],
                     [
                         'name' => '怪盗',
+                        'votable' => true,
+                        'peepable' => false,
+                        'swappable' => true,
                     ],
                 ]
             ],
@@ -46,6 +61,9 @@ class LoadRoleData implements FixtureInterface, OrderedFixtureInterface
                 'roles' => [
                     [
                         'name' => '吊人',
+                        'votable' => true,
+                        'peepable' => false,
+                        'swappable' => false,
                     ],
                 ],
             ]
@@ -56,6 +74,9 @@ class LoadRoleData implements FixtureInterface, OrderedFixtureInterface
             foreach($record['roles'] as $roleRecord) {
                 $role = new Role();
                 $role->setName($roleRecord['name']);
+                $role->setVotable($roleRecord['votable']);
+                $role->setPeepable($roleRecord['peepable']);
+                $role->setSwappable($roleRecord['swappable']);
                 $role->setRoleGroup($roleGroup);
                 $roleGroup->addRole($role);
                 $manager->persist($roleGroup);
